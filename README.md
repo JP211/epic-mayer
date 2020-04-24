de8c26c94a2cea096c5b1d288b6aa1e58738562c
Jessica Cockerell <jlpeters211@gmail.com>
2020-04-24 10:14:12 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# epic-mayer
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import StarRating from "./StarRating";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <StarRating />
+    </div>
+  );
+}
